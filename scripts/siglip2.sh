fa9d9aa9b081b66d7fa7f6580d7ae8e605d69ba9
Shrenik <shrenikborad@gmail.com>
2026-03-09 16:33:37 -0400

    feat: add siglip2 script for computing embeddings with batch processing

---
+++ scripts/siglip2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -o logs/uwisc_batch_%j.out
+#SBATCH -e logs/uwisc_batch_%j.err
+#SBATCH -N 1
+#SBATCH -p superChip
+#SBATCH --cpus-per-task=72
+#SBATCH --mem=128G
+#SBATCH --mail-user=s.borad@gwu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 24:00:00
+
+cd $HOME/contrails_eccv/groundcam-contrail-detection
+
+REPO_DIR="$HOME/felidae-image-search"
+DATA_DIR="$REPO_DIR/data"
+IMAGE_DIR="$DATA_DIR/images"
+METADATA_ZIP="$DATA_DIR/felidae_conservation_fund_2020_2025.zip"
+METADATA_JSON="$REPO_DIR/felidae_conservation_fund_2020_2025.json"
+
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate contrail_detection
+
+
+python scripts/compute_embeddings_siglip2.py \
+    --mode local \
+    --image_dir "$IMAGE_DIR" \
+    --metadata "$METADATA_JSON" \
+    --output_dir "$DATA_DIR/siglip2"
